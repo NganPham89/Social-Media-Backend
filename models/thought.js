fb06d40749e829eb5cef4441f94426ed254ac7e3
NganPham89 <algarvian2001@yahoo.com>
2023-05-25 02:40:44 -0400

    created models and schemas

---
+++ models/thought.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+const dayjs = require("dayjs");
+const reactionSchema = require("./reaction");
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlenght: 1,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => dayjs(date).format('DD/MM/YYYY'),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    },
+);
+
+thoughtSchema.virtual("reactionCount")
+    .get(function () {
+        return this.reactions.length;
+    });
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
