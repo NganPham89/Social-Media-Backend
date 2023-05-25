bb60cf98cf82d076421abedcf50630cdaf6f3cac
NganPham89 <algarvian2001@yahoo.com>
2023-05-25 01:57:29 -0400

    setting up file structures

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: "^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$",
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "thought",
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "user",
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
+    }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
